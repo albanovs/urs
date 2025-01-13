4e51fb1aa91f99b797d6b77070384db763649a36
albanovs <albvnovs@gmail.com>
2025-01-13 18:29:20 +0600

    api change

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-export const api = axios.create({
-    baseURL: 'http://localhost:5000'
-})
-
 // export const api = axios.create({
-//     baseURL: 'https://integration-sov-kg.onrender.com'
-// })  
+//     baseURL: 'http://localhost:5000'
+// })
+
+export const api = axios.create({
+    baseURL: 'https://integration-sov-kg.onrender.com'
+})  
